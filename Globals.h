8630a99444960274c1789e436617015ea036993b
Eli Haber <elihaber@gmail.com>
2021-01-27 02:14:21 +0200

    Changed loggers to singleton

--- Globals.h
+++ Globals.h
@@ -2,13 +2,9 @@
 
 #include "Logger.h"
 
-#define MAINLOG(x) MainLog.out << x << std::endl; MainLog.out.flush();
-#define MAINLOG_NNL(x) MainLog.out << x; MainLog.out.flush();
-#define INPUTLOG(x) InputLog.out << x << std::endl; InputLog.out.flush();
-#define INPUTLOG_NNL(x) InputLog.out << x; InputLog.out.flush();
-#define OUTPUTLOG(x) OutputLog.out << x << std::endl; OutputLog.out.flush();
-#define OUTPUTLOG_NNL(x) OutputLog.out << x; OutputLog.out.flush();
-
-static Logger MainLog("SaschaMainLog.txt");
-static Logger InputLog("SaschaInputLog.txt");
-static Logger OutputLog("SaschaOutputLog.txt");
+#define MAINLOG(x) Logger::getInstance().mainout << x << std::endl; Logger::getInstance().mainout.flush();
+#define MAINLOG_NNL(x) Logger::getInstance().mainout << x; Logger::getInstance().mainout.flush();
+#define INPUTLOG(x) Logger::getInstance().inputout << x << std::endl; Logger::getInstance().inputout.flush();
+#define INPUTLOG_NNL(x) Logger::getInstance().inputout << x; Logger::getInstance().inputout.flush();
+#define OUTPUTLOG(x) Logger::getInstance().outputout << x << std::endl; Logger::getInstance().outputout.flush();
+#define OUTPUTLOG_NNL(x) Logger::getInstance().outputout << x; Logger::getInstance().outputout.flush();
--- Logger.h
+++ Logger.h
@@ -1,10 +1,21 @@
 #pragma once
 
-#include <string>
 #include <fstream>
 
 class Logger {
 public:
-    Logger(const std::string & path);
-    std::ofstream out;
+    static Logger & getInstance() {
+        static Logger instance;
+        return instance;
+    }
+
+private:
+    Logger();
+
+public:
+    Logger(Logger const &) = delete;
+    void operator =(Logger const &) = delete;
+    std::ofstream mainout;
+    std::ofstream inputout;
+    std::ofstream outputout;
 };
